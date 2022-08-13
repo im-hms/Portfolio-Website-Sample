3a9b2f06204fec5703540957f9614a63fc6913a0
H.M.S <99068400+im-hms@users.noreply.github.com>
2022-08-13 22:57:00 +0500

    Add files via upload

---
+++ userinfo.php
@@ -0,0 +1,23 @@
+<?php
+
+$con = mysqli_connect('localhost', 'root');
+
+if($con){
+    echo "Connection Successful";
+}else{
+    echo "No Connection";
+}
+mysqli_select_db($con, 'portfolioinfo');
+
+$user = $_POST['user'];
+$email = $_POST['email'];
+$project = $_POST['project'];
+$comment = $_POST['comment'];
+
+$query = "insert into projecttable (user, email, project, comment)
+values ('$user', '$email', '$project', '$comment') ";
+
+mysqli_query($con, $query);
+header ('location:index.php');
+
+?>
